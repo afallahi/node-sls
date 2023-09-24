e9992ce918ba488d00a5c94622982b5db1b12227
Afshin <fallahi_a@yahoo.com>
2023-09-24 01:40:11 -0400

    added user service and dummy signup handler

---
+++ user-service/app/handler.ts
@@ -0,0 +1,17 @@
+import { APIGatewayProxyEventV2 } from "aws-lambda";
+
+
+export const Signup = async (event: APIGatewayProxyEventV2) => {
+    console.log(`Event: ${event}`);
+
+    return {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify ({
+            message: "response from signup",
+            daya: {}
+        }),
+    };
+};
